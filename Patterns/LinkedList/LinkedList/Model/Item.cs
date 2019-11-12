62e9b695e9f89e4f0dbb5c61f2bd453a1235c64c
dimanaumow123 <dimanaumow123@gmail.com>
2019-11-13 01:48:04 +0300

    TODO: Add Iterator for LinkedList

---
+++ Patterns/LinkedList/LinkedList/Model/Item.cs
@@ -0,0 +1,26 @@
+﻿namespace LinkedList.Model
+{
+    public class Item<T>
+    {
+        private T data = default(T); 
+
+        public T Data
+        {
+            get => data;
+            set{if (value != null)
+                data = value; }
+        }
+        
+        public Item<T> Next { get; set; }
+
+        public Item(T data)
+        {
+            Data = data; 
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString(); 
+        }
+    }
+}
---
+++ Patterns/LinkedList/LinkedList/Model/LinkedList.cs
@@ -0,0 +1,73 @@
+﻿namespace LinkedList.Model
+{
+    public class LinkedList<T>
+    {
+        public Item<T> Head { get; private set; }
+        
+        public Item<T> Teil { get; private set; }
+        
+        public int Count { get; private set; }
+
+        public LinkedList()
+        {
+            Head = null;
+            Teil = null;
+            Count = 0; 
+        }
+
+        public LinkedList(T data)
+        {
+            var item = new Item<T>(data);
+            SetHeadAndTeil(item);
+        }
+
+        public void Add(T data)
+        {
+            var item = new Item<T>(data);
+            if (Teil != null)
+            {
+                Teil.Next = item;
+                Teil = item;
+                Count++; 
+            }
+            else
+            {
+                SetHeadAndTeil(item);
+            }
+        }
+
+        public void Remove(T data)
+        {
+            if(Head != null)
+            {
+                if (Head.Data.Equals(data))
+                {
+                    Head = Head.Next;
+                    Count--;
+                    return; 
+                }
+
+                var current = Head.Next;
+                var previous = Head;
+
+                while (current != null)
+                {
+                    if (current.Data.Equals(data))
+                    {
+                        previous.Next = current.Next;
+                        Count--; 
+                    }
+
+                    previous = current;
+                    current = current.Next; 
+                }
+            }
+        }
+        private void SetHeadAndTeil(Item<T> item)
+        {
+            Head = item;
+            Teil = item;
+            Count = 1;
+        }
+    }
+}
